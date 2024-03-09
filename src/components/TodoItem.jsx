d148b57a9ba49d6e51cb7bf6c5f7579b85a0623f
Arslan Javaid <aja118379@gmail.com>
2024-03-09 21:12:22 +0500

    Applied simple Event Handler for delete button

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,13 +2,21 @@ import styles from './TodoItem.module.css'
 
 const TodoItem1 = ({todoName, todoDate}) => {
 
+  let itemDelete = (todoName) => {
+    console.log(`${todoName} deleted`)
+  }
+
   return (
     <>
       <div className="row container mt-2 text-center">
         <div className="col-6">{todoName}</div>
         <div className="col-4">{todoDate}</div>
         <div className="col-2">
-          <button type="button" className={`${styles.deleteBtn} btn btn-danger`}>
+          <button 
+            type="button" 
+            className={`${styles.deleteBtn} btn btn-danger`}
+            onClick={() => itemDelete(todoName)}
+          >
             Delete
           </button>
         </div>
