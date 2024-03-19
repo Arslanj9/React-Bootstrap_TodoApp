8e821933125f705b5ac78280f7b17de52a92d728
Arslan Javaid <aja118379@gmail.com>
2024-03-19 20:27:27 +0500

    Used Reducer inside App component to manage states more efficiently

--- src/App.jsx
+++ src/App.jsx
@@ -3,53 +3,81 @@ import AddTodo from "./components/AddTodo";
 import TodoItems from "./components/TodoItems";
 import Container from "./components/Container";
 import WelcomeMsge from "./components/WelcomeMsge";
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import { todoItemContext } from "./store/todoItemContext";
 
-const App = () => {
-  // let initialItems = [
-  //   {
-  //     name: "Buy Milk",
-  //     date: "23/3/2024"
-  //   },
-  //   {
-  //     name: "Go to Gym",
-  //     date: "13/2/2024"
-  //   },
-  //   {
-  //     name: "Have Dinner",
-  //     date: "3/3/2024"
-  //   }
-  // ];
-  // const [ items, setItems ] = useState(innitialItems)
 
-  const [items, setItems] = useState([]);
+const itemsInnitialState = [
+    {
+      name: "Buy Milk",
+      date: "23/3/2024"
+    },
+    {
+      name: "Go to Gym",
+      date: "13/2/2024"
+    },
+    {
+      name: "Have Dinner",
+      date: "3/3/2024"
+    }
+  ];
+
+const itemsReducer = (itemsState, action) => {
+  
+    let newItemsState = itemsState;
+    if(action.type === 'ADD_ITEM') {
+      let dateParts = action.payload.date.split("-");
+      let formattedDate = dateParts[2] + "/" + dateParts[1] + "/" + dateParts[0];
+  
+      newItemsState = [...itemsState, { name: action.payload.name, date: formattedDate }];
+      console.log(newItemsState)
+    }else if (action.type === 'DELETE_ITEM') {
+      newItemsState = itemsState.filter((obj) => obj.name !== action.payload.name);
+    }
+    return newItemsState;
+}
+
+const App = () => {
+
+  const [ itemsState, dispatchItems ] = useReducer(itemsReducer, itemsInnitialState)
+
 
   const onSubmit = (todoName, todoDate) => {
-    var dateParts = todoDate.split("-");
-    var formattedDate = dateParts[2] + "/" + dateParts[1] + "/" + dateParts[0];
-
-    const newTodoItems = [...items, { name: todoName, date: formattedDate }];
-    setItems(newTodoItems);
+    const newTodoItems = {
+      type: "ADD_ITEM",
+      payload: {
+        name: todoName,
+        date: todoDate
+      }
+    }
+    dispatchItems(newTodoItems)
   };
 
+
   const handleDeleteItem = (todoName) => {
-    const newItems = items.filter((obj) => obj.name !== todoName);
-    setItems(newItems);
+
+    const deleteTodoItem = {
+      type: "DELETE_ITEM",
+      payload: {
+        name: todoName
+      }
+    }
+
+    dispatchItems(deleteTodoItem)
   };
 
   const handleEdit = (todoName, newText) => {
-    setItems((prevTodos) =>
-      prevTodos.map((todo) =>
-        todo.name == todoName ? { ...todo, name: newText } : todo
-      )
-    );
+    // setItems((prevTodos) =>
+    //   prevTodos.map((todo) =>
+    //     todo.name == todoName ? { ...todo, name: newText } : todo
+    //   )
+    // );
   };
 
   return (
     <todoItemContext.Provider 
       value={{
-        allItems: items, 
+        allItems: itemsState, 
         deleteItem: handleDeleteItem, 
         addItem: onSubmit,
         editItem: handleEdit
@@ -59,12 +87,8 @@ const App = () => {
         <AppName />
         <AddTodo />
 
-        {items.length == 0 ? (<WelcomeMsge />) : (<TodoItems />)}
-        
-        {/* We can also achieve same results by this method given below */}
-        {/* --------- Logical AND operator --------- */}
-        {/* { items.length == 0 && <WelcomeMsge/> }
-      <TodoItems items={items} /> */}
+        {itemsState.length == 0 ? (<WelcomeMsge />) : (<TodoItems />)}
+
       </Container>
     </todoItemContext.Provider>
   );
